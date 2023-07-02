570673cd715daf0f2e63377899a84a13dadc500e
Simão Pedro Sá Cunha <61991247+simaocunha71@users.noreply.github.com>
2023-07-02 21:44:08 +0100

    sidebar so renderiza uma vez em app.js

--- src/pages/Certifications/Certifications.js
+++ src/pages/Certifications/Certifications.js
@@ -1,18 +1,9 @@
-import React, { useState } from 'react';
-import Toolbar from '../../components/Toolbar/Toolbar';
-import Sidebar from '../../components/Sidebar/Sidebar';
+import React from 'react';
 import Footer from '../../components/Footer/Footer';
 
 const Certifications = () => {
-  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
-
-  const toggleSidebar = () => {
-    setIsSidebarOpen(!isSidebarOpen);
-  };
   return (
     <>
-      <Toolbar toggleSidebar={toggleSidebar} isSidebarOpen={isSidebarOpen} />
-      <Sidebar isOpen={isSidebarOpen} toggleSidebar={toggleSidebar} />
       <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', fontWeight: 'bold', height: '100vh' }}>
         <span>Certifications</span>
       </div>
