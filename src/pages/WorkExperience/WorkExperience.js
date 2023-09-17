9944f83d37ba05fbf960747e81cb7dc74773a9a4
simaocunha71 <simaopscunha@outlook.pt>
2023-09-17 15:18:46 +0100

    update work experience

--- src/pages/WorkExperience/WorkExperience.js
+++ src/pages/WorkExperience/WorkExperience.js
@@ -9,17 +9,24 @@ const workExperiences = [
     company: 'Accenture Portugal',
     jobTitle: 'Summer Internship',
     duration: 'Jul 2022 - Aug 2022',
-    description: 'Worked with a cloud engineering team:\n' +
-      'Google Cloud Platform basics\n' +
-      'Data Fusion instance creation and uploading a JSON file with a custom pipeline configuration using a Cloud Build file written in YAML\n' +
-      'Agile Scrum methodology applied during the entire internship',
+    description: 
+    `Google Cloud Platform basics
+     Data Fusion instance creation and uploading a JSON file with a custom pipeline configuration using a Cloud Build file written in YAML
+     Agile Scrum methodology applied during the entire internship`,
+    keywords: ['Google Cloud Platform', 'Agile Scrum'],
     logo: accentureLogo,
   },
   {
     company: 'Bosch Portugal',
     jobTitle: 'Summer Internship',
-    duration: 'Jul 2023 - TBD',
-    description: 'TBD',
+    duration: 'Jul 2023 - Sep 2023',
+    description: 
+    `Applied machine learning and deep learning techniques to develop a system for detecting delaminations.
+     Implemented Machine learning models using Python libraries such as Scikit-learn (Sklearn) and Deep Learning models using Deep Learning Halcon by MVTEC to build robust detection algorithms.
+     Conducted image processing tasks using Halcon from MVTEC to enhance data preprocessing.
+     Contributed to internal software development projects.
+     Gained foundational knowledge in computer vision.`,
+    keywords: ['Machine Learning', 'Deep Learning', 'Image Processing', 'Computer Vision'],
     logo: boschLogo,
   },
   // Add more work experiences as needed
@@ -36,15 +43,18 @@ const WorkExperience = () => {
             <h3>{experience.company}</h3>
             <p>{experience.jobTitle}</p>
             <p>{experience.duration}</p>
-            {experience.description.split('\n').map((item, itemIndex) => (
-              itemIndex === 0 ? (
-                <p key={itemIndex}>{item}</p>
-              ) : (
-                <ul key={itemIndex}>
-                  <li>{item}</li>
-                </ul>
-              )
-            ))}
+            <ul>
+              {experience.description.split('\n').map((item, itemIndex) => (
+                <li key={itemIndex}>{item.trim()}</li>
+              ))}
+            </ul>
+            <div className="keywords-container">
+              {experience.keywords.map((keyword, keywordIndex) => (
+                <span key={keywordIndex} className="keyword-rectangle">
+                  {keyword}
+                </span>
+              ))}
+            </div>
           </div>
         </div>
       ))}
